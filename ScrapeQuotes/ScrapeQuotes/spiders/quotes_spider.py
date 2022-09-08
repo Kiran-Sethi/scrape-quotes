1ad860a376d554bdb462dbb30fd319ceff43abe5
KIRANSETHI17 <sethikiran1995@gmail.com>
2022-09-08 23:27:13 +0530

    spider added for scraping quotes

---
+++ ScrapeQuotes/ScrapeQuotes/spiders/quotes_spider.py
@@ -0,0 +1,27 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    #identifies the Spider. It must be unique within a project, that is, you can’t set the same name for different Spiders.
+    name = "quotes"
+
+    def start_requests(self):
+        ##must return iterable of requests or a generator function for requests
+        urls = [
+            'https://quotes.toscrape.com/page/1/',
+            'https://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        # page = response.url.split("/")[-2]
+        # filename = f'quotes-{page}.html'
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+        # self.log(f'Saved file {filename}')
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
